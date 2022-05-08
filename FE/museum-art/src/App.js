0ec3beda53b30c4105d7f918482f836073e44941
AleksandarStanisic01 <astanisic@ideacompiler.com>
2022-05-08 12:35:01 +0200

    bug fixes

--- FE/museum-art/src/App.js
+++ FE/museum-art/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("http://localhost:5000/api/data/tree/10");
+      const goted = await res.json();
+      setData(goted);
+      return goted;
+    };
+    getData();
+  }, []);
+
+  console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      jedi govna
+      {data?.map((element) => {
+        return (
+          <>
+            <p>{element.id}</p>
+            <p>{element.name}</p>
+            <p>
+              {element.collection.map((c) => {
+                return <li>name:{c.name}</li>;
+              })}
+            </p>
+          </>
+        );
+      })}
+    </>
   );
 }
 
