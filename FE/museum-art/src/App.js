5ebea7b6a4383aa36c2e628a0e6e6b9ae89baf25
AleksandarStanisic01 <astanisic@ideacompiler.com>
2022-05-09 18:15:44 +0200

    new feats

--- FE/museum-art/src/App.js
+++ FE/museum-art/src/App.js
@@ -1,39 +1,63 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import { Container, Typography, Box, Button } from "@mui/material/";
+import { Paper } from "@mui/material/";
 
 function App() {
   const [data, setData] = useState();
+  const [americanState, setAmericanState] = useState("0");
 
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch("http://localhost:5000/api/data/tree/10");
+      const res = await fetch("http://localhost:5000/api/data/tree/20");
       const goted = await res.json();
       setData(goted);
       return goted;
     };
     getData();
   }, []);
-
-  console.log(data);
-
+  console.log(americanState);
   return (
     <>
-      <h3>FE App</h3>
-      {data?.map((element) => {
-        return (
-          <>
-            <p>{element.id}</p>
-            <p>{element.name}</p>
-            <p>
-              {element.collection.map((c) => {
-                return <li>name:{c.name}</li>;
-              })}
-            </p>
-          </>
-        );
-      })}
+      <Container>
+        <Paper elevation={3} style={{ backgroundColor: "#f1f1f1" }}>
+          <Typography
+            component={"h1"}
+            variant={"h4"}
+            style={{ color: "red" }}
+            padding={1}
+          >
+            FE App
+          </Typography>
+        </Paper>
+
+        {data?.map((element) => {
+          return (
+            <Paper>
+              {/*<Typography>{element.id}</Typography>*/}
+              <Typography element={"h2"} variant={"h5"}>
+                {element.name}
+              </Typography>
+              <Paper>
+                {element.collection?.map((c) => {
+                  return (
+                    <Typography variant={"p"} element={"p"}>
+                      <Box key={Math.random()}>
+                        <b>Name:</b>
+                        <Button onClick={() => setAmericanState(c.id)}>
+                          {c.name}
+                        </Button>
+                      </Box>
+                    </Typography>
+                  );
+                })}
+              </Paper>
+            </Paper>
+          );
+        })}
+      </Container>
+      <Box>{americanState}</Box>
     </>
   );
 }
-
 export default App;
