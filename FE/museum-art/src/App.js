f3c6f3dff2a99a2f57138d429e974915e847fc8e
AleksandarStanisic01 <astanisic@ideacompiler.com>
2022-05-13 18:12:15 +0200

    adding pictures

--- FE/museum-art/src/App.js
+++ FE/museum-art/src/App.js
@@ -4,10 +4,31 @@ import { Container, Typography, Box, Button } from "@mui/material/";
 import { Paper } from "@mui/material/";
 import { TreeItem, TreeView } from "@material-ui/lab";
 
-function App() {
-  const [dataUs, setUsData] = useState();
-  const [americanState, setAmericanState] = useState("0");
+//http://localhost:5000/api/data/colection/
 
+function App() {
+  const [collectionData, setCollection] = useState();
+  const [dataUs, setUsData] = useState();
+  const [euData, setEuData] = useState();
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("http://localhost:5000/api/data/colection/");
+      const result = await res.json();
+      setCollection(result);
+      return result;
+    };
+    getData();
+  }, []);
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("http://localhost:5000/api/data/tree/20");
+      const result = await res.json();
+      setEuData(result);
+      return result;
+    };
+    getData();
+  }, []);
   useEffect(() => {
     const getData = async () => {
       const res = await fetch("http://localhost:5000/api/data/tree/10");
@@ -18,58 +39,121 @@ function App() {
     getData();
   }, []);
 
+  let collectionImages = collectionData?.collection.map((c) => {
+    return (
+      <div key={c.url}>
+        <img src={`${c.url}`} alt="" />
+      </div>
+    );
+  });
+
+  const handleClick = (id) => {
+    console.log(collectionData?.collection);
+    console.log(id);
+  };
+  console.log(collectionData);
   return (
     <>
-      <Container>
+      <Container h={100}>
         <Paper elevation={3} style={{ backgroundColor: "#f1f1f1" }}>
           <Typography
-            component={"h4"}
+            component={"h5"}
             m={1}
-            variant={"h4"}
+            variant={"h5"}
             style={{ color: "gray" }}
             padding={1}
           >
             Museum Art
           </Typography>
         </Paper>
-
-        {dataUs?.map((element) => {
-          return (
-            <TreeView
-              aria-label="file system navigator"
-              sx={{
-                height: 240,
-                flexGrow: 1,
-                maxWidth: 100,
-                overflowY: "auto",
-              }}
-            >
-              <TreeItem nodeId="1" label={element.name.toUpperCase()}>
-                <Paper>
-                  {/*<Typography>{element.id}</Typography>*/}
+        <Box m={1} style={{ display: "flex" }}>
+          {dataUs?.map((element) => {
+            return (
+              <TreeView
+                aria-label="file system navigator"
+                sx={{
+                  height: 240,
+                  flexGrow: 1,
+                  maxWidth: 100,
+                  overflowY: "auto",
+                }}
+              >
+                <TreeItem nodeId="1" label={element.name.toUpperCase()}>
                   <Paper>
-                    {element.collection?.map((c) => {
-                      return (
-                        <Typography variant={"p"} element={"p"}>
-                          <Box key={Math.random()}>
-                            <Button
-                              style={{ color: "gray" }}
-                              onClick={() => setAmericanState(c.id)}
-                            >
-                              {c.name}
-                            </Button>
-                          </Box>
-                        </Typography>
-                      );
-                    })}
+                    {/*<Typography>{element.id}</Typography>*/}
+                    <Paper>
+                      {element.collection?.map((c) => {
+                        return (
+                          <Typography variant={"p"} element={"p"}>
+                            <Box key={Math.random()}>
+                              <Button
+                                style={{ color: "gray" }}
+                                onClick={() => handleClick(c.id)}
+                              >
+                                {c.name}
+                              </Button>
+                            </Box>
+                          </Typography>
+                        );
+                      })}
+                    </Paper>
                   </Paper>
-                </Paper>
-              </TreeItem>
-            </TreeView>
-          );
-        })}
+                </TreeItem>
+              </TreeView>
+            );
+          })}
+          {euData?.map((element) => {
+            return (
+              <TreeView
+                aria-label="file system navigator"
+                sx={{
+                  height: 240,
+                  flexGrow: 1,
+                  maxWidth: 100,
+                  overflowY: "auto",
+                }}
+              >
+                <TreeItem nodeId="1" label={element.name.toUpperCase()}>
+                  <Paper>
+                    {/*<Typography>{element.id}</Typography>*/}
+                    <Paper>
+                      {element.collection?.map((c) => {
+                        return (
+                          <Typography variant={"p"} element={"p"}>
+                            <Box key={Math.random()}>
+                              <Button
+                                style={{ color: "gray" }}
+                                onClick={() => handleClick(c.id)}
+                              >
+                                {c.name}
+                              </Button>
+                            </Box>
+                          </Typography>
+                        );
+                      })}
+                    </Paper>
+                  </Paper>
+                </TreeItem>
+              </TreeView>
+            );
+          })}
+          <Box
+            m={1}
+            style={{
+              backgroundColor: "orange",
+              width: "60%",
+              height: "100vh",
+            }}
+          >
+            <Paper
+              elevation={3}
+              style={{ backgroundColor: "#f1f1f1", height: "100vh" }}
+            >
+              <Box width={"40%"}> {collectionImages}</Box>
+            </Paper>
+          </Box>
+        </Box>
       </Container>
-      <Box style={{ backgroundColor: "orange" }}>{americanState}</Box>
     </>
   );
 }
